18b83473d8500a9958bf212d450ad773439368a6
Aleksei Semin <alllexsm@gmail.com>
2018-01-09 11:15:04 +0100

    Add day 17 extra

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Day17 where
 
-import Data.List (elemIndex, foldl')
+import Data.List (foldl')
 
 day17 :: String -> Int
 day17 s = xs !! ((i + 1) `mod` length xs)
@@ -10,12 +12,14 @@ day17 s = xs !! ((i + 1) `mod` length xs)
         (i, xs) = foldl' sub (0, [0]) [1..2017]
 
 day17extra :: String -> Int
-day17extra s = xs !! ((zIndex + 1) `mod` length xs)
+day17extra s = let (_, zNextValFinal, _) = foldl' sub (0, 0, 0) [1..50 * 10 ^ 6] in zNextValFinal
     where 
         shift = read s :: Int
-        sub (i, xs) n = let j = (i + shift) `mod` length xs in (j + 1, insertAfter xs (j + 1) n)
-        (i, xs) = foldl' sub (0, [0]) [1..50 * 10 ^ 6]
-        (Just zIndex) = 0 `elemIndex` xs
+        sub (zIndex, zNextVal, curIndex) n = (zIndexNew, zNextValNew, insertAfterIndex + 1)
+            where
+                !insertAfterIndex = (curIndex + shift) `mod` n
+                !zIndexNew = if insertAfterIndex < zIndex then zIndex + 1 else zIndex
+                !zNextValNew = if insertAfterIndex == zIndex then n else zNextVal
 
 insertAfter :: [a] -> Int -> a -> [a]
 insertAfter xs 0 a = a:xs
